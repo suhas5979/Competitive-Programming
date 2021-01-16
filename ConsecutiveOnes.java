d2ad610a592f7e456a5c6709eaf957f1dde925fc
suhas5979 <suhassuryavanshi5979@gmail.com>
2021-01-16 20:51:25 +0530

    initial commit

---
+++ ConsecutiveOnes.java
@@ -0,0 +1,25 @@
+/**
+ * ConsecutiveOnes
+ */
+
+public class ConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 1, 0, 2, 1, 3, 4, 2, 0, 9, 0, 2, 1, 3, 0, 5 };
+        System.out.println(findConsecutiveOnes(arr));
+    }
+
+    public static int findConsecutiveOnes(int[] arr) {
+        int max = 0;
+        int currentMax = 0;
+        for (int i : arr) {
+            if (i == 1) {
+                currentMax++;
+                max = Math.max(currentMax, max);
+            } else {
+                currentMax = 0;
+            }
+        }
+        return max;
+    }
+}
